42326344f37ab9444c8aff111a2de50d35d28437
Walter Ribeiro <wribeiro@testreach.com>
2022-08-08 23:21:23 -0300

    Finish github authentication and stripe payment

---
+++ src/pages/api/webhooks.ts
@@ -0,0 +1,73 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from "stream";
+import Stripe from "stripe";
+import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
+
+async function buffer(readable: Readable) {
+  const chunks = [];
+
+  for await (const chunk of readable) {
+    chunks.push(
+      typeof chunk === "string" ? Buffer.from(chunk) : chunk
+    );
+  }
+
+  return Buffer.concat(chunks);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  }
+}
+
+const stripeEvents = new Set([
+  'checkout.session.completed'
+])
+
+const webhooks = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const buf = await buffer(req)
+    const secret = req.headers['stripe-signature'];
+
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET);
+    } catch (err) {
+      return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    switch (event.type) {
+      case 'checkout.session.completed':
+        const paymentIntent = event.data.object as Stripe.Checkout.Session;
+
+        await saveSubscription(
+          paymentIntent.subscription.toString(),
+          paymentIntent.customer.toString(),
+          true,
+        )
+
+        break;
+      case 'customer.subscription.updated':
+      case 'customer.subscription.deleted':
+        const subscription = event.data.object as Stripe.Subscription;
+
+        await saveSubscription(
+          subscription.id,
+          subscription.customer.toString(),
+        )
+
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    return res.json({ received: true });
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method not allowed");
+  }
+}
+
+export default webhooks;
